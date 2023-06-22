1a3b3e2d2c9211edf9cf3d1fecc2480486fcd536
Menkveld <menke.veerman@gmail.com>
2023-06-22 17:40:40 +0200

    questdb host update

--- server-deployment/kafka-initialization/src/utils/configLoader.ts
+++ server-deployment/kafka-initialization/src/utils/configLoader.ts
@@ -41,7 +41,7 @@ interface appConfig {
 
 const config: appConfig = {
     questdb: {
-        host: process.env.QUESTDB_HOST ?? 'questdb',
+        host: process.env.QUESTDB_HOST ?? 'http://questdb',
         port: parseInt(process.env.QUESTDB_PORT ?? '9000'),
         table_name: process.env.QUESTDB_TABLE_NAME ?? 'THINGY_LOCATION_BEACONS_MERGED'
     },
