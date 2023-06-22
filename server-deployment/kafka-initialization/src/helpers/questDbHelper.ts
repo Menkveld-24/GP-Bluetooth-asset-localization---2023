89aaeddb056387c89fd28e0613790ce657f59c66
Menkveld <menke.veerman@gmail.com>
2023-06-22 17:38:59 +0200

    query error

--- server-deployment/kafka-initialization/src/helpers/questDbHelper.ts
+++ server-deployment/kafka-initialization/src/helpers/questDbHelper.ts
@@ -20,21 +20,7 @@ export async function executeQuestDB (query: string): Promise<void> {
 
 export async function createMergedLocationBeaconsTable (): Promise<void> {
     log(`Creating QuestDB table: ${config.questdb.table_name}`);
-    const query = `CREATE TABLE ${config.questdb.table_name} (
-            mac SYMBOL INDEX, 
-            humidity double, 
-            fwVersion int, 
-            rssi int, 
-            temperature double, 
-            co2_ppm int, 
-            battery double, 
-            rollover int, 
-            latitude double, 
-            longitude double, 
-            timestamp TIMESTAMP
-        )
-        TIMESTAMP(timestamp)
-        PARTITION BY DAY;`;
+    const query = `CREATE TABLE ${config.questdb.table_name} ( mac SYMBOL INDEX,  humidity double,  fwVersion int,  rssi int,  temperature double,  co2_ppm int,  battery double,  rollover int,  latitude double,  longitude double,  timestamp TIMESTAMP ) TIMESTAMP(timestamp) PARTITION BY DAY;`;
 
     await executeQuestDB(query);
     log(`Created QuestDB table: ${config.questdb.table_name}`);
