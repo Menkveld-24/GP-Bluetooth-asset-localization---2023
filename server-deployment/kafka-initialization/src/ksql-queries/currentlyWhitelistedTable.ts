7ec2b2332c8dc73da874905133f5354fb589c547
Menkveld <menke.veerman@gmail.com>
2023-06-22 16:33:59 +0200

    whitelist topic creation name fix

--- server-deployment/kafka-initialization/src/ksql-queries/currentlyWhitelistedTable.ts
+++ server-deployment/kafka-initialization/src/ksql-queries/currentlyWhitelistedTable.ts
@@ -1,5 +1,5 @@
 import config from '../utils/configLoader';
 
-const QUERY = `CREATE SOURCE TABLE whitelisted_thingies WITH (KAFKA_TOPIC='${config.kafka.topics.whitelist.name}', VALUE_FORMAT='AVRO', KEY_FORMAT='AVRO');`;
+const QUERY = `CREATE SOURCE TABLE ${config.ksql.query_output_names.whitelist} WITH (KAFKA_TOPIC='${config.kafka.topics.whitelist.name}', VALUE_FORMAT='AVRO', KEY_FORMAT='AVRO');`;
 
 export default QUERY;
