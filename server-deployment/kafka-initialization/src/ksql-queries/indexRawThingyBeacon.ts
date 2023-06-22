c7922efa854ea731e510c60a3a991c2c1060ea92
Menkveld <menke.veerman@gmail.com>
2023-06-22 17:13:45 +0200

    stream name fix

--- server-deployment/kafka-initialization/src/ksql-queries/indexRawThingyBeacon.ts
+++ server-deployment/kafka-initialization/src/ksql-queries/indexRawThingyBeacon.ts
@@ -2,11 +2,11 @@ import { type OptionalExtraParams } from 'ksqldb-client';
 import config from '../utils/configLoader';
 
 // eslint-disable-next-line
-const QUERY = 'CREATE STREAM ${STREAM_NAME} AS SELECT *, STRUCT(mac:=mac, rollover:=rollover) AS index FROM ${KAFKA_TOPIC};';
+const QUERY = 'CREATE STREAM ${STREAM_NAME} AS SELECT *, STRUCT(mac:=mac, rollover:=rollover) AS index FROM ${SRC_STREAM};';
 const options: OptionalExtraParams = {
     sessionVariables: {
-        STREAM_NAME: `${config.kafka.topics.raw_beacons.name}_STREAM`,
-        KAFKA_TOPIC: config.kafka.topics.raw_beacons.name
+        STREAM_NAME: config.ksql.query_output_names.raw_beacons,
+        SRC_STREAM: `${config.kafka.topics.raw_beacons.name}_STREAM`
     }
 };
 
