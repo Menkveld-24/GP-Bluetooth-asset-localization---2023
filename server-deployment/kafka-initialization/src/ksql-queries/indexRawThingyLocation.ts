d9358c88a88b3baf2675588054c57027b5e3e922
Menkveld <menke.veerman@gmail.com>
2023-06-22 17:10:31 +0200

    typo

--- server-deployment/kafka-initialization/src/ksql-queries/indexRawThingyLocation.ts
+++ server-deployment/kafka-initialization/src/ksql-queries/indexRawThingyLocation.ts
@@ -2,7 +2,7 @@ import { type OptionalExtraParams } from 'ksqldb-client';
 import config from '../utils/configLoader';
 
 // eslint-disable-next-line
-const QUERY = 'CREATE STREAM ${STREAM_NAME} AS SELECT *, STRUCT(mac:=mac, rollover:=rollover) AS index FROM ${TOPIC_NAME};';
+const QUERY = 'CREATE STREAM ${STREAM_NAME} AS SELECT *, STRUCT(mac:=mac, rollover:=rollover) AS index FROM ${KAFKA_TOPIC};';
 const options: OptionalExtraParams = {
     sessionVariables: {
         STREAM_NAME: `${config.kafka.topics.raw_locations.name}_STREAM`,
