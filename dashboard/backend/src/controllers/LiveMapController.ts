121072efd2631018bf541b40fabc3d941e749eb3
Menkveld <menke.veerman@gmail.com>
2023-06-29 11:13:36 +0200

    caching live thingies

--- dashboard/backend/src/controllers/LiveMapController.ts
+++ dashboard/backend/src/controllers/LiveMapController.ts
@@ -1,3 +1,4 @@
+import { getOrSetJson } from '@services/CachingService';
 import { getLatestThingys } from '@services/QuestdbService';
 import express, { type Request, type Response } from 'express';
 
@@ -5,7 +6,9 @@ const router = express.Router();
 
 // eslint-disable-next-line
 router.get('/last-locations', async (req: Request, res: Response) => {
-    const thingys = await getLatestThingys();
+    const thingys = getOrSetJson('last-locations', async () => {
+        return await getLatestThingys();
+    });
     res.success(thingys, `Last locations: ${Date.now()}`);
 });
 
